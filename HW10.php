665675a475b6f34697c83fdc5a8944162ff55f13
vasilich <vasylich68@gmail.com>
2022-10-13 17:36:31 +0300

    all 6 task

--- HW10.php
+++ HW10.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $array = [];
 $integer = rand(1,400);
 $NUM = 50;
@@ -10,3 +11,30 @@ echo"<br>";
 print_r('сума всіх елементів масиву - '. $arrSum);
 echo"<br>";
 print_r('среднее арифметическое всех элементов массива   - '. $arrSum/$NUM );
+*/
+
+$size = 50;
+$array = [];
+for ($i = 1; $i <= 50; $i++){
+    $array[$i] =rand(1,400);
+}
+$arrSum = array_sum($array);
+var_dump($array);
+echo"<br>";
+print_r('сума всіх елементів масиву - '. $arrSum);
+echo"<br>";
+print_r('среднее арифметическое всех элементов массива   - '. $arrSum / count($array) );
+echo"<br>";
+print_r('мінімальний елемент массиву - ' . min($array) );
+echo"<br>";
+print_r('максимальний элемент массива - '. max($array) );
+echo"<br>";
+sort($array);
+foreach ($array as $value){
+    echo "сртуваня по зростанню.$value. <br>";
+}
+echo"<br>";
+rsort($array);
+foreach ($array as $value){
+    echo "сортування по спаданню.$value. <br>";
+}
